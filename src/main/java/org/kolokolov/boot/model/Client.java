655572f4820e0a16916b90db805714ef0789d429
kolokolov <kolokolov_a@ukr.net>
2017-08-09 23:08:59 +0300

    Modify Client entity
    Modify Client entity class in order
    to get rid of Json annotations

--- src/main/java/org/kolokolov/boot/model/Client.java
+++ src/main/java/org/kolokolov/boot/model/Client.java
@@ -1,8 +1,5 @@
 package org.kolokolov.boot.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -10,23 +7,29 @@ import javax.persistence.Id;
 @Entity
 public class Client {
 
-    @JsonProperty
     @Id
     @GeneratedValue
     private int id;
-    @JsonProperty("first_name")
     private String firstName;
-    @JsonProperty("last_name")
     private String lastName;
 
     public Client() {}
 
-    @JsonCreator
-    public Client(@JsonProperty("id") int id,
-                  @JsonProperty("first_name") String firstName,
-                  @JsonProperty("last_name") String lastName) {
+    public Client(int id, String firstName, String lastName) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 }
